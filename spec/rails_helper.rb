f10981b3f38e9538ba0ee0842117701ebf587360
Wakim Jraige <wakim.jraige@gmail.com>
2017-02-08 15:04:12 -0200

    Configure postgres and add Database Cleaner.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,5 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-  config.render_views
   config.include FactoryGirl::Syntax::Methods
 end
